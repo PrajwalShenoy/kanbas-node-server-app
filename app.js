1410ce5b0342ce6d59b2cea4bc8755cafb4410b9
Prajwal Shenoy <prajwalkpshenoy@gmail.com>
2023-12-02 02:00:01 -0500

    a6 changes

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 
-mongoose.connect("mongodb://localhost:27017/kanbas");
+// mongoose.connect("mongodb://localhost:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL,
 }
 ));
 const sessionOptions = {
@@ -22,6 +24,13 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }  
 app.use(
     session(sessionOptions)
 );
